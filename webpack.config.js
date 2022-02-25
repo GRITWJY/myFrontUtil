1c33c7156835f76bad559b9a67ca86a47f9ce647
WJYGRIT <1435408225@qq.com>
2022-02-25 12:50:35 +0800

    xfaf

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,20 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+	mode:'development',
 	entry: {
 		index: './src/index.js',
 		print: './src/print.js'
 	},
+	devServer:{
+		static:'./dist'
+	},
 	plugins:[
 		new HtmlWebpackPlugin({
-			title:'管理输出'
+			title:'development'
 		})
 	],
+	devtool: 'inline-source-map',
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist'),
